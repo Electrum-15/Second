6a29309bde232511fe622d4e9542f95ed3028d4e
ShivendraSingh <singhbargahi@gmail.com>
2022-09-13 16:24:50 +0530

    Content in Cpp files has been put

---
+++ FirstCode.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"FirstCode.."<<endl;
+    return 0;
+}
---
+++ ThirdCode.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"ThirdCode.."<<endl;
+    return 0;
+}
